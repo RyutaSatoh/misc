8002b0ee683a22cd97493ad75992278bd868407b
Ryuta Satoh <ryu@MBA2011.local>
2013-06-23 11:53:04 +0900

    add formula of google test

---
+++ formula/googletest.rb
@@ -0,0 +1,11 @@
+require 'formula'
+class Wget < Formula
+  homepage 'http://code.google.com/p/googletest/'
+  url 'http://googletest.googlecode.com/files/gtest-1.6.0.zip'
+  md5 '4577b49f2973c90bf9ba69aa8166b786'
+
+  def install
+    system "./configure --prefix=#{prefix}"
+    system 'make install'
+  end
+end
